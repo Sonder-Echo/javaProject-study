0ed879dccdf5f8fe93b758ccd915f4144145c887
sonder <382547989@qq.com>
2025-07-30 20:41:26 +0800

    智能云图库平台项目初始化完成

---
+++ yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.sonder.yunpicturebackend.exception;
+
+import com.sonder.yunpicturebackend.common.BaseResponse;
+import com.sonder.yunpicturebackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ */
+@RestControllerAdvice //环绕切面注解
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException: " + e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> businessExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException: " + e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
