0727d34a0a346bb266653408cd00479572e8cfec
sonder <382547989@qq.com>
2025-07-30 20:41:26 +0800

    智能云图库平台项目初始化完成

---
+++ yun-picture/yun-picture-frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ConfigProvider :locale="zhCN">
+    <div id="app">
+      <BasicLayout />
+    </div>
+  </ConfigProvider>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { healthUsingGet } from '@/api/mainController.ts'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import { ConfigProvider } from 'ant-design-vue'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+healthUsingGet()
+  .then((res) => {
+    console.log('健康检查成功', res)
+  })
+  .catch((err) => {
+    console.error('健康检查失败', err)
+  })
+</script>
+
+<style scoped></style>
