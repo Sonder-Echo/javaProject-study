26e00d6ad788ec1b4ce87d94531d9829b5a1099a
sonder <382547989@qq.com>
2025-07-31 23:11:04 +0800

    feat(backend):用户管理-用户登录接口

--- yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/UserService.java
+++ yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/UserService.java
@@ -3,6 +3,9 @@ package com.sonder.yunpicturebackend.service;
 import com.sonder.yunpicturebackend.model.dto.UserRegisterRequest;
 import com.sonder.yunpicturebackend.model.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.sonder.yunpicturebackend.model.vo.LoginUserVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author Sonder
@@ -19,10 +22,26 @@ public interface UserService extends IService<User> {
     Long userRegister(UserRegisterRequest userRegisterRequest);
 
     /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request   http请求
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取脱敏后的用户信息
+     * @param user  用户实体类
+     * @return  脱敏后的用户信息
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
      * 获取加密密码
-     * @param userPassword
-     * @param salt
-     * @return
+     * @param userPassword  用户输入的密码
+     * @return  加密后的密码
      */
     String getEncryptPassword(String userPassword);
 }
