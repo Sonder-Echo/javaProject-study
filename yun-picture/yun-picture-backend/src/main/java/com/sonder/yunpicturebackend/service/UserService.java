d35edce72148b28ca619e0ed93e4e9297f6c7d06
sonder <382547989@qq.com>
2025-07-31 22:39:13 +0800

    feat(backend):用户管理-用户注册接口

--- yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/UserService.java
+++ yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.sonder.yunpicturebackend.service;
 
+import com.sonder.yunpicturebackend.model.dto.UserRegisterRequest;
 import com.sonder.yunpicturebackend.model.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -10,4 +11,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface UserService extends IService<User> {
 
+    /**
+     * 用户注册
+     * @param userRegisterRequest  用户注册信息
+     * @return  用户 id
+     */
+    Long userRegister(UserRegisterRequest userRegisterRequest);
+
+    /**
+     * 获取加密密码
+     * @param userPassword
+     * @param salt
+     * @return
+     */
+    String getEncryptPassword(String userPassword);
 }
--- yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/impl/UserServiceImpl.java
+++ yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/impl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.sonder.yunpicturebackend.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sonder.yunpicturebackend.exception.BusinessException;
+import com.sonder.yunpicturebackend.exception.ErrorCode;
+import com.sonder.yunpicturebackend.model.dto.UserRegisterRequest;
 import com.sonder.yunpicturebackend.model.entity.User;
+import com.sonder.yunpicturebackend.model.enums.UserRoleEnum;
 import com.sonder.yunpicturebackend.service.UserService;
 import com.sonder.yunpicturebackend.mapper.UserMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 /**
 * @author Sonder
@@ -15,6 +23,52 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Override
+    public Long userRegister(UserRegisterRequest userRegisterRequest) {
+        //1.校验参数
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        String checkPassword = userRegisterRequest.getCheckPassword();
+        if(StrUtil.hasBlank(userAccount,userPassword,checkPassword)){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
+        }
+        if(userAccount.length() < 4){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账户过短");
+        }
+        if(userPassword.length() < 8 || checkPassword.length() < 8){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户密码过短");
+        }
+        if(!userPassword.equals(checkPassword)){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "两次输入的密码不一致");
+        }
+        //2.检查用户账户是否存在
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        Long count = this.baseMapper.selectCount(queryWrapper);
+        if(count > 0){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账户已存在");
+        }
+        //3.密码加密、加盐
+        String encryptPassword = getEncryptPassword(userPassword);
+        //4.插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        user.setUserName("LaLa");
+        user.setUserRole(UserRoleEnum.USER.getValue());
+        boolean saveResult = this.save(user);
+        if(!saveResult){
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "注册失败");
+        }
+        return user.getId();
+    }
+
+    @Override
+    public String getEncryptPassword(String userPassword) {
+        //加盐，混淆密码
+        final String SALT = "sonder";
+        return DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+    }
 }
 
 
