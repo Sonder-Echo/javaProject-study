13d17b2ef4d87b077a8d149f0f218b3963100de4
sonder <382547989@qq.com>
2025-08-11 17:55:36 +0800

    feat(frontend):图片管理-图片上传与创建接口

---
+++ yun-picture/yun-picture-frontend/src/pages/AddPicturePage.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="addPicturePage">
+    <h2 style="margin-bottom: 16px">创建图片</h2>
+    <!-- 图片上传组件 -->
+    <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+    <!-- 图片信息表单 -->
+    <a-form name="pictureForm" layout="vertical" :model="pictureForm" @finish="handleSubmit">
+      <a-form-item name="name" label="名称">
+        <a-input v-model:value="pictureForm.name" placeholder="请输入图片名称" allow-clear />
+      </a-form-item>
+      <a-form-item name="introduction" label="简介">
+        <a-textarea
+          v-model:value="pictureForm.introduction"
+          placeholder="请输入图片简介"
+          :autoSize="{ minRows: 2, maxRows: 5}"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item name="category" label="分类">
+        <a-auto-complete
+          v-model:value="pictureForm.category"
+          placeholder="请输入图片分类"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item name="tags" label="标签">
+        <a-select
+          v-model:value="pictureForm.tags"
+          mode="tags"
+          placeholder="请输入图片标签">
+        </a-select>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" style="width: 100%">创建</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PictureUpload from '@/components/PictureUpload.vue'
+import { reactive, ref } from 'vue'
+import { userLoginUsingPost } from '@/api/userController.ts'
+import { message } from 'ant-design-vue'
+import { editPictureUsingPost } from '@/api/pictureController.ts'
+import { useRouter } from 'vue-router'
+
+const picture = ref<API.PictureVO>()
+const pictureForm = reactive<API.PictureEditRequest>({})
+
+/**
+ * 图片上传成功
+ * @param newPicture
+ */
+const onSuccess = (newPicture: API.PictureVO) => {
+  picture.value = newPicture
+  pictureForm.name = newPicture.name
+}
+
+const router = useRouter();
+
+/**
+ * 提交表单
+ * @param values
+ */
+const handleSubmit = async (values: any) => {
+  const pictureId = picture.value?.id
+  if (!pictureId) {
+    message.error('请上传图片')
+    return
+  }
+  try {
+    const res = await editPictureUsingPost({
+      id: pictureId,
+      ...values, // ...可以将对象的每个值拷贝到新的对象中
+    })
+    // 操作成功
+    if (res.data.code === 0 && res.data.data) {
+      // 登录成功, 保存用户登录态到全局状态中
+      message.success('创建成功')
+      // 跳转到图片详情页面
+      router.push({
+        path: `/picture/${pictureId}`
+      })
+    } else {
+      // 创建失败
+      message.error('创建失败，' + res.data.message)
+    }
+  } catch (e) {
+    message.error('创建失败，' + e.message)
+  }
+}
+</script>
+
+<style scoped>
+#addPicturePage {
+  max-width: 720px;
+  margin: 0 auto;
+}
+</style>
