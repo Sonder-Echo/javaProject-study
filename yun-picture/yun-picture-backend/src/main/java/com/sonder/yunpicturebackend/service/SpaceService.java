33a159743054b1851c5c1003e3fe591cf42007e8
sonder <382547989@qq.com>
2025-08-16 13:52:31 +0800

    feat(backend):空间模块-空间表的设计与空间管理接口

---
+++ yun-picture/yun-picture-backend/src/main/java/com/sonder/yunpicturebackend/service/SpaceService.java
@@ -0,0 +1,58 @@
+package com.sonder.yunpicturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sonder.yunpicturebackend.model.dto.space.SpaceQueryRequest;
+import com.sonder.yunpicturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sonder.yunpicturebackend.model.vo.SpaceVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Sonder
+ * @description 针对表【space(图片)】的数据库操作Service
+ * @createDate 2025-08-15 21:13:10
+ */
+public interface SpaceService extends IService<Space> {
+    /**
+     * 校验图片
+     *
+     * @param space
+     * @param add   是否为创建时校验
+     */
+    void validSpace(Space space, boolean add);
+
+    /**
+     * 获取图片包装类（单条）
+     *
+     * @param space
+     * @param request
+     * @return
+     */
+    SpaceVO getSpaceVO(Space space, HttpServletRequest request);
+
+    /**
+     * 获取图片分页包装类（分页）
+     *
+     * @param spacePage
+     * @param request
+     * @return
+     */
+    Page<SpaceVO> getSpaceVOPage(Page<Space> spacePage, HttpServletRequest request);
+
+    /**
+     * 获取查询对象
+     *
+     * @param spaceQueryRequest
+     * @return
+     */
+    QueryWrapper<Space> getQueryWrapper(SpaceQueryRequest spaceQueryRequest);
+
+    /**
+     * 根据空间级别填充空间对象
+     * @param space
+     */
+    void fillSpaceBySpaceLevel(Space space);
+
+}
